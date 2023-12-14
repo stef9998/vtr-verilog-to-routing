9ea63226a1c55dbb418df44d7c0b6d47e1cb718f
stef9998 <stef9998@mail.de>
2023-12-14 21:38:35 +0100

    removed unnecessary calculation

--- FPGARoutingSim/Resistor.java
+++ FPGARoutingSim/Resistor.java
@@ -1,20 +1,22 @@
 public class Resistor {
     final Fault fault;  // fault of resistor/memristor
 
-    // Constructor
     public Resistor(FaultRates faultRates) {
         double rnd = Math.random() * 100;   // random number between 0 and 100 to select the fault
 
         // if rnd is between 0 and faultrate of SA0
         if (0 <= rnd && rnd < faultRates.getFaultRate(Fault.SA0)){
+        if (rnd < faultRates.getFaultRate(Fault.SA0)){
             fault = Fault.SA0;  // fault is SA0
         }
         // else if rnd is between faultrate of SA0 and SA1
         else if (faultRates.getFaultRate(Fault.SA0) <= rnd && rnd < faultRates.getFaultRate(Fault.SA1)){
+        else if (rnd < faultRates.getFaultRate(Fault.SA1)){
             fault = Fault.SA1;  // fault is SA1
         }
         // else if rnd is between faultrate of SA1 and UD
         else if (faultRates.getFaultRate(Fault.SA1) <= rnd && rnd < faultRates.getFaultRate(Fault.UD)){
+        else if (rnd < faultRates.getFaultRate(Fault.UD)){
             fault = Fault.UD;   // fault is UD
         } else {
             fault = Fault.FF;   // memristor is fault free
