8f3c716d86cf879de37ca2609304af9e4b05daba
stef9998 <stef9998@mail.de>
2023-12-29 09:37:16 +0100

    moved fault to field

--- FPGARoutingSim/Switch.java
+++ FPGARoutingSim/Switch.java
@@ -6,21 +6,26 @@ import org.jgrapht.graph.DefaultEdge;
  */
 public class Switch extends DefaultEdge {
     final private MemCell controlCell;  // control cell of the switch/edge
+    final private Fault memFault;
 
     /**
      * set control Cell
      */
     public Switch(MemCell controlCell){
         this.controlCell = controlCell;
+        memFault = this.controlCell.getCellFault();
     }
 
     /**
-     * returns cell fault of the contained memCell
+     * returns cell fault of the memory cell controlling this switch
      */
     public Fault getFault(){
-        return controlCell.getCellFault();
+        return memFault;
     }
 
+    /**
+     * returns the memory cell controlling this switch
+     */
     public MemCell getControlCell(){
         return controlCell;
     }
